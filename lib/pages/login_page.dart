764dd1651287d5b8e6e04fd94f34f99e7a2a63ab
sofiaramirez <sofiaa.ramirez016@gmail.com>
2024-03-13 15:01:05 -0600

    cambios de diseño de UI y se agregaron funciones para mostrar errores en el llenado de textfield

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,150 +1,177 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:free_yourself_app/components/googletile_login.dart';
 import 'package:free_yourself_app/components/signin_button.dart';
 import 'package:free_yourself_app/components/textfield_login.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
 
-  //text editing controller
-  final usernameController = TextEditingController();
+class LoginPage extends StatefulWidget {
+  final Function()? onTap;
+  const LoginPage({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // text editing controllers
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  //sign user in method
-  void signUserIn() {}
+  // sign user in method
+  void signUserIn() async {
+    // show loading circle
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
 
+    // try sign in
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      // pop the loading circle
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      // pop the loading circle
+      Navigator.pop(context);
+      //show error message 
+      showErrorMessage(e.code);
+    }
+  }
+
+  // error message to user
+  void showErrorMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.teal,
+          title: Center(
+            child: Text(
+              message,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 50),
-          
-                //logo
-                Icon(
-                  Icons.lock,
-                  size: 100,
-                ),
-                SizedBox(height: 50),
-                Text(
-                  '¡Bienvenido de vuelta!',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50),
+                        
+                  //logo
+                  Icon(
+                    Icons.lock,
+                    size: 100,
                   ),
-                ),
-                SizedBox(height: 25),
-          
-                //username textfield
-                MyTextField(
-                  controller: usernameController,
-                  hintText: 'Email',
-                  obscureText: false,
-                ),
-          
-          
-                SizedBox(height: 10),
-          
-                //password textfield
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  obscureText: true,
-                ),
-          
-                SizedBox(height: 10),
-          
-                //forgot password?
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                  SizedBox(height: 50),
+                  Text(
+                    '¡Bienvenido de vuelta!',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                        
+                  //email textfield
+                  MyTextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false,
+                  ),
+                        
+                        
+                  SizedBox(height: 10),
+                        
+                  //password textfield
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
+                        
+                      
+                  SizedBox(height: 25),
+                        
+                  //Sign in button
+                  SignInButton(
+                    text: "Iniciar Sesión",
+                    onTap: signUserIn,
+                  ),
+                        
+                  SizedBox(height: 50),
+                        
+                  //or continue with
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                        
+                        
+
+                        
+                  SizedBox(height: 30),
+                        
+                  //registrate ahora
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        '¿Olvidaste la contraseña?',
-                        style: TextStyle(color: Colors.grey[600]),
+                        '¿No tienes una cuenta?',
+                        style: TextStyle(color: Colors.grey[700]),
                       ),
-                    ],
-                  ),
-                ),
-          
-                SizedBox(height: 25),
-          
-                //Sign in button
-                SignInButton(
-                  onTap: signUserIn,
-                ),
-          
-                SizedBox(height: 50),
-          
-                //or continue with
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      SizedBox(width: 4),
+                      GestureDetector(
+                        onTap: widget.onTap,
                         child: Text(
-                          'Continuar con',
-                          style: TextStyle(color: Colors.grey[700]),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
+                          'Registrate ahora!',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ],
-                  ),
-                ),
-          
-                SizedBox(height: 30),
-          
-                //entrar con google
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //google
-                    GoogleTileLogin(imagePath: 'lib/images/google.png'),
-                  ],
-                ),
-          
-                SizedBox(height: 30),
-          
-                //registrate ahora
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '¿No tienes una cuenta?',
-                      style: TextStyle(color: Colors.grey[700]),
-                    ),
-                    SizedBox(width: 4),
-                    Text(
-                      'Registrate ahora!',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                )
-          
-              ],
+                  )
+                        
+                ],
+              ),
             ),
           ),
         ),
